396399521188733f01deadbfed72f7c21b81694b
amruthasalike <amrutha@gmail.com>
2018-02-28 19:07:39 +0530

    added script files

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#building
+cd JavaSample/SpringMVCSecurityXML
+mvn clean install
+
+#renaming war file
+cd target
+echo "build successful"
+mv SpringMVCSecurityXML.war amrutha.war
+
+#copying war file in remote tomcat webapps
+scp amrutha.war amrutha@54.244.209.186:/var/lib/tomcat8/webapps
+if [ $? = 0 ]
+then
+	echo "deploy successful"
+fi
+
+#checking whether java app is deployed
+firefox 54.244.209.186:8080/amrutha
+
+
