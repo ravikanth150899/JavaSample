64aa237c4024446afbac3d254d347426683ef7e2
AMRUTHASALIKE <31771178+AMRUTHASALIKE@users.noreply.github.com>
2018-03-02 11:30:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 mv ./SpringMVCSecurityXML/target/SpringMVCSecurityXML.war ./SpringMVCSecurityXML/target/amrutha1.war
 
 #copying war file in remote tomcat webapps
-scp amrutha1.war amrutha@54.244.209.186:/var/lib/tomcat8/webapps
+scp ./SpringMVCSecurityXML/target/amrutha1.war amrutha@54.244.209.186:/var/lib/tomcat8/webapps
 if [ $? = 0 ]
 then
 	echo "deploy successful"
