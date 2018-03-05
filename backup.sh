5ce517d09a644b1867afd47dbf8c8f168590f85e
AMRUTHASALIKE <31771178+AMRUTHASALIKE@users.noreply.github.com>
2018-03-05 16:10:33 +0530

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,11 @@
+ssh amrutha@54.244.209.186<<'EOF'
+if [ $(ls /home/amrutha/backup | wc -l) -ne 0 ]
+then 
+	touch -r /home/amrutha/backup/ammu_curr.war	/home/amrutha/backup/ammu_prev.war
+	mv /home/amrutha/backup/ammu_curr.war	/home/amrutha/backup/ammu_prev.war
+fi 
+touch -r /var/lib/tomcat8/webapps/ammu.war /home/amrutha/backup/ammu_curr.war
+cp /var/lib/tomcat8/webapps/ammu.war /home/amrutha/backup/ammu_curr.war
+EOF
+
+exit
